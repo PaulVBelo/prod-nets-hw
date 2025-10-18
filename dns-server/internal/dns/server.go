9c830a7b4dcebb60c2cda759900ee066d830c138
Pavel Belousov <belousovpv600@rambler.ru>
2025-10-18 15:31:55 +0300

    Added cache

--- dns-server/internal/dns/server.go
+++ dns-server/internal/dns/server.go
@@ -3,23 +3,34 @@ package dns
 import (
 	"context"
 	"dns-server/internal/config"
+	"log"
 	"net"
 	"strings"
+	"sync"
 	"time"
 
 	miekg_dns "github.com/miekg/dns"
 )
 
+type cacheEntry struct {
+	msg *miekg_dns.Msg
+	expiry time.Time
+}
+
 type Server struct {
 	cfg           *config.Config
 	client        *miekg_dns.Client
 	upstreamAddrs []string
+
+	cache map[string]cacheEntry
+	mu sync.RWMutex
 }
 
 func NewServer(cfg *config.Config) (*Server, error) {
 	s := &Server{
 		cfg:    cfg,
 		client: &miekg_dns.Client{Net: "udp", Timeout: 3 * time.Second},
+		cache: make(map[string]cacheEntry),
 	}
 
 	if len(cfg.Upstream) > 0 {
@@ -29,7 +40,6 @@ func NewServer(cfg *config.Config) (*Server, error) {
 	}
 
 	s.upstreamAddrs = sanitizeUpstreams(cfg.Listen, s.upstreamAddrs)
-
 	return s, nil
 }
 
@@ -84,15 +94,47 @@ func (s *Server) lookupA(name string) (string, bool) {
 	return "", false
 }
 
+func (s *Server) cacheKey(r *miekg_dns.Msg) string {
+	if len(r.Question) == 0 {
+		return ""
+	}
+	q := r.Question[0]
+	return strings.ToLower(miekg_dns.Fqdn(q.Name)) + ":" + miekg_dns.TypeToString[q.Qtype]
+}
+
 func (s *Server) forward(w miekg_dns.ResponseWriter, r *miekg_dns.Msg) {
+	key := s.cacheKey(r)
+
+	s.mu.RLock()
+	entry, ok := s.cache[key]
+	s.mu.RUnlock()
+
+	if ok && time.Now().Before(entry.expiry) {
+		log.Printf("used cache: %s", key)
+		cached := entry.msg.Copy()
+		cached.SetReply(r)
+		_ = w.WriteMsg(cached)
+		return
+	}
+
+	// Ищем в апстрим
 	for _, ns := range s.upstreamAddrs {
 		resp, _, err := s.client.Exchange(r, ns)
 		if err == nil && resp != nil {
+			// Кешируем
+			s.mu.Lock()
+			s.cache[key] = cacheEntry{
+				msg: resp.Copy(),
+				expiry: time.Now().Add(time.Duration(s.cfg.TTL) * time.Second),
+			}
+			s.mu.Unlock()
+
 			_ = w.WriteMsg(resp)
 			return
 		}
 	}
 
+	// SERVFAIL
 	m := new(miekg_dns.Msg)
 	m.SetReply(r)
 	m.Rcode = miekg_dns.RcodeServerFailure
@@ -125,7 +167,31 @@ func sanitizeUpstreams(listen string, ns []string) []string {
 	return out
 }
 
+func (s *Server) startCacheCleaner(ctx context.Context) {
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <- ctx.Done():
+			return
+
+		case <- ticker.C:
+		now := time.Now()
+		s.mu.Lock()
+			for k, v := range s.cache {
+				if now.After(v.expiry) {
+					delete(s.cache, k)
+				}
+			}
+		s.mu.Unlock()
+		}
+	}
+}
+
 func (s *Server) Run(ctx context.Context) error {
+	go s.startCacheCleaner(ctx)
+
 	udp := &miekg_dns.Server{Addr: s.cfg.Listen, Net: "udp", Handler: miekg_dns.HandlerFunc(s.ServeDNS)}
 	tcp := &miekg_dns.Server{Addr: s.cfg.Listen, Net: "tcp", Handler: miekg_dns.HandlerFunc(s.ServeDNS)}
 
